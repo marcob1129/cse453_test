38695cd8844c9bfa7c27103e34e03fc0be0f7a74
marcobia <marcobia@Ubuntu.myguest.virtualbox.org>
2026-03-23 10:06:12 -0400

    testing

---
+++ ros2_ws/src/gps_processor/src/gps_processor.cpp
@@ -0,0 +1,37 @@
+#include <rclcpp/rclcpp.hpp>                    
+#include <sensor_msgs/msg/nav_sat_fix.hpp>  
+
+
+class GPSProcessor : public rclcpp::Node
+{
+public:
+    GPSProcessor() : Node("gps_processor")     
+    {
+        
+        subscription_ = this->create_subscription<sensor_msgs::msg::NavSatFix>("/fix",10,std::bind(&GPSProcessor::gpsCallback,this,std::placeholders::_1));
+    }
+
+private:
+    rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr subscription_;
+
+    void gpsCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
+    {
+        double lat = msg->latitude;
+        double lon = msg->longitude;
+        double alt = msg->altitude;
+
+        RCLCPP_INFO(this->get_logger(),
+            "GPS: Lat = %f, Lon = %f, Alt = %f",
+            lat, lon, alt);
+
+    }
+};
+
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);                             
+    rclcpp::spin(std::make_shared<GPSProcessor>());      
+    rclcpp::shutdown();                                   
+    return 0;                                             
+}
